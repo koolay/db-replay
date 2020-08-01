fe35686cac9554cb9a61c853198ac4dbfba272d7
koolay <koolay@163.com>
2020-08-01 22:50:34 +0800

    add proxy

--- go.mod
+++ go.mod
@@ -5,9 +5,10 @@ go 1.14
 require (
 	github.com/go-sql-driver/mysql v1.4.1
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
-	github.com/pkg/errors v0.9.1 // indirect
-	github.com/siddontang/go-log v0.0.0-20180807004314-8d05993dda07
-	github.com/siddontang/go-mysql v1.1.0
+	github.com/pingcap/errors v0.11.0
+	github.com/pkg/errors v0.9.1
+	github.com/siddontang/go-mysql v1.1.1-0.20200720140823-f5700ccec199
+	github.com/sirupsen/logrus v1.6.0
 	google.golang.org/appengine v1.6.6 // indirect
 	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
 )
--- main.go
+++ main.go
@@ -1,64 +1,91 @@
 package main
 
 import (
-	"net"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"path"
+	"runtime"
+	"strings"
 
-	"github.com/siddontang/go-log/log"
-	"github.com/siddontang/go-mysql/mysql"
-	"github.com/siddontang/go-mysql/server"
-	"github.com/siddontang/go-mysql/test_util/test_keys"
-
-	"crypto/tls"
-	"time"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/koolay/db-replay/proxy"
+	"github.com/sirupsen/logrus"
 )
 
-type RemoteThrottleProvider struct {
-	*server.InMemoryProvider
-	delay int // in milliseconds
-}
+const defaultVersion = "5.7.28"
 
-func (m *RemoteThrottleProvider) GetCredential(username string) (password string, found bool, err error) {
-	time.Sleep(time.Millisecond * time.Duration(m.delay))
-	return m.InMemoryProvider.GetCredential(username)
+func newLogger(levelName string, hooks ...logrus.Hook) *logrus.Logger {
+	level, err := logrus.ParseLevel(levelName)
+	if err != nil {
+		level = logrus.ErrorLevel
+	}
+
+	logFormatter := &logrus.TextFormatter{
+		TimestampFormat: "2006-01-02 15:04:05.000",
+		FullTimestamp:   true,
+		CallerPrettyfier: func(f *runtime.Frame) (string, string) {
+			s := strings.Split(f.Function, ".")
+			funcname := s[len(s)-1]
+			_, filename := path.Split(f.File)
+			return funcname, fmt.Sprintf("%s:%v", filename, f.Line)
+		},
+	}
+
+	logger := logrus.New()
+	logger.SetLevel(level)
+	logger.SetOutput(os.Stdout)
+	logger.SetFormatter(logFormatter)
+	logger.SetReportCaller(true)
+
+	for _, hook := range hooks {
+		logger.AddHook(hook)
+	}
+	return logger
 }
 
 func main() {
-	l, _ := net.Listen("tcp", "127.0.0.1:3309")
-	// user either the in-memory credential provider or the remote credential provider (you can implement your own)
-	//inMemProvider := server.NewInMemoryProvider()
-	//inMemProvider.AddUser("root", "123")
-	remoteProvider := &RemoteThrottleProvider{server.NewInMemoryProvider(), 10 + 50}
-	remoteProvider.AddUser("root", "123")
-
-	var tlsConf = server.NewServerTLSConfig(test_keys.CaPem,
-		test_keys.CertPem, test_keys.KeyPem, tls.VerifyClientCertIfGiven)
-
-	for {
-		c, err := l.Accept()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		go func() {
-			// Create a connection with user root and an empty password.
-			// You can use your own handler to handle command here.
-			// const version = "8.0.12"
-			const version = "5.7.0"
-
-			svr := server.NewServer(version,
-				mysql.DEFAULT_COLLATION_ID, mysql.AUTH_CACHING_SHA2_PASSWORD, test_keys.PubPem, tlsConf)
-			conn, err := server.NewCustomizedConn(c, svr, remoteProvider, &MysqlHandler{})
-
-			if err != nil {
-				log.Errorf("Connection error: %v", err)
-				return
-			}
-
-			for {
-				if herr := conn.HandleCommand(); herr != nil {
-					log.Fatal(herr)
-				}
-			}
-		}()
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	cfg := proxy.Config{
+		Addr:     "0.0.0.0:3309",
+		User:     "root",
+		Password: "123",
+		TargetConnection: proxy.Connection{
+			Host:     "127.0.0.1",
+			Port:     3306,
+			User:     "root",
+			Password: "dev",
+			Database: "myapp",
+		},
 	}
+
+	logger := newLogger("DEBUG")
+	ser := proxy.NewProxyServer(defaultVersion, cfg)
+	handler, err := proxy.NewMysqlHandler(cfg.TargetConnection, logger)
+	if err != nil {
+		logger.WithError(err).Error("failed to new handler")
+		return
+	}
+
+	proxier, err := proxy.NewProxy(cfg, logger, ser, handler)
+	if err != nil {
+		logger.WithError(err).Error("failed to new proxy")
+		return
+	}
+
+	go func() {
+		log.Println("start db proxy")
+		if err := proxier.Start(); err != nil {
+			logger.WithError(err).Error("failed to start proxy")
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	if err := proxier.Shutdown(); err != nil {
+		log.Println(err)
+	}
+	log.Println("shutdown")
 }
